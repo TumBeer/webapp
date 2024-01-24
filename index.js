a92b845249c853c5090204a619caec2345555d4f
TumBeer <tumloveliuyifei@gmail.com>
2024-01-24 10:59:13 +0900

    Tum

--- index.js
+++ index.js
@@ -199,6 +199,6 @@ wss.on('connection', function connection(ws) {
 
 
 const PORT = 3000;
-server.listen(PORT, 'websocket-v2jj.onrender.com', () => console.log(`Lisening on port :3000`))
+server.listen(PORT, () => console.log(`Lisening on port :3000`))
 
 
